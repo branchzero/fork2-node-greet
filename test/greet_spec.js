bb23702396634d15b86a0d375ef16130c929d336
BranchZero <branchzero@gmail.com>
2014-06-24 01:31:45 +0800

    warm up2

---
+++ test/greet_spec.js
@@ -0,0 +1,10 @@
+var greet = require("../");
+var mygreet = greet('BranchZero', 1);
+describe('greet', function(){
+	it("should greet a preson by name", function(){
+		expect(mygreet).to.contain('BranchZero');
+	});
+        it("should greet a person flirtatiously if drunk", function(){
+                expect(mygreet).to.contain(', you look sexy today');
+        });
+});
